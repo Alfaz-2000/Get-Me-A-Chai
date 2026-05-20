51d417a0d61e32a8d6eb531237cfb62833cb9ac1
Alfaz-2000 <alfazappu74@gmail.com>
2026-05-20 20:52:51 +0530

    first day of project some ui and setup

---
+++ app/layout.js
@@ -0,0 +1,36 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Get Me A Chai - super chat system for developers",
+  description: "people can help developers by funding them or buying them a chai",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+        <div className="min-h-[87vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+        {children}
+        </div>
+        <Footer/>
+        </body>
+    </html>
+  );
+}
---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-black text-white flex justify-center items-center px-4 h-16'>
+<p>CopyRight &copy; HasAlfaz - All Rights Reserved! </p>
+    </footer>
+  )
+}
+
+export default Footer
